4587e1da05fc147da9cffc05c00b02b36e2a1a58
juhovh <juhovh@37e7b450-0b1e-11de-ba4d-252e5d5a8945>
2009-05-16 16:44:57 +0000

    Add license header to the RawSocket.cs file

--- server/RawSocket.cs
+++ server/RawSocket.cs
@@ -1,3 +1,20 @@
+/**
+ *  NABLA - Automatic IP Tunneling and Connectivity
+ *  Copyright (C) 2009  Juho Vähä-Herttua
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 using System;
 using System.Net;
@@ -24,10 +41,10 @@ public class RawSocket {
 	private static extern int rawsock_recvfrom(int sockfd, byte[] buf, int offset, int len, byte[] sockaddr, ref int addrlen, ref int err);
 
 	[DllImport("rawsock")]
-	private static extern void rawsock_destroy(int sockfd);
+	private static extern string rawsock_strerror(int errno);
 
 	[DllImport("rawsock")]
-	private static extern string rawsock_strerror(int errno);
+	private static extern void rawsock_destroy(int sockfd);
 
 
 	public RawSocket(AddressFamily addressFamily, int protocol) {
