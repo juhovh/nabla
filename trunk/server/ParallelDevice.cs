ed66045e500357d28b5111dc1284f507d460e166
juhovh <juhovh@37e7b450-0b1e-11de-ba4d-252e5d5a8945>
2009-06-01 09:45:45 +0000

    Add untested ARP and NDSol request sending for address resolution

--- trunk/server/ParallelDevice.cs
+++ trunk/server/ParallelDevice.cs
@@ -56,6 +56,7 @@ namespace Nabla {
 		Dictionary<IPAddress, IPConfig> _subnets
 			= new Dictionary<IPAddress, IPConfig>();
 
+		Object _arplock = new Object();
 		Dictionary<IPAddress, byte[]> _arptable
 			= new Dictionary<IPAddress, byte[]>();
 
@@ -83,15 +84,24 @@ namespace Nabla {
 		public void SendPacket(byte[] data, int offset, int datalen) {
 			int version = (data[offset] >> 4) & 0x0f;
 
+			IPAddress src;
 			IPAddress dest;
 			bool multicast;
 			if (version == 4) {
 				byte[] ipaddr = new byte[4];
+
 				Array.Copy(data, offset+26, ipaddr, 0, 4);
+				src = new IPAddress(ipaddr);
+
+				Array.Copy(data, offset+30, ipaddr, 0, 4);
 				dest = new IPAddress(ipaddr);
 				multicast = (ipaddr[0] < 224 && ipaddr[0] > 239);
 			} else if (version == 6) {
 				byte[] ipaddr = new byte[16];
+
+				Array.Copy(data, offset+22, ipaddr, 0, 16);
+				src = new IPAddress(ipaddr);
+
 				Array.Copy(data, offset+38, ipaddr, 0, 16);
 				dest = new IPAddress(ipaddr);
 				multicast = dest.IsIPv6Multicast;
@@ -134,11 +144,34 @@ namespace Nabla {
 					dest = route;
 				}
 
-				if (_arptable.ContainsKey(dest)) {
-					hwaddr = _arptable[dest];
-				} else {
-					/* FIXME: Attempt to make an ARP/ND request instead */
-					hwaddr = new byte[] { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff };
+				lock (_arplock) {
+					/* Wait one second between requests */
+					TimeSpan span = new TimeSpan(0, 0, 1);
+
+					for (int i=0; i<3; i++) {
+						DateTime startTime = DateTime.Now;
+
+						while (!_arptable.ContainsKey(dest)) {
+							if (dest.AddressFamily == AddressFamily.InterNetwork) {
+								sendARPRequest(src, dest);
+							} else {
+								sendNDSol(src, dest);
+							}
+							/* FIXME: Attempt to make an ARP/ND request */
+
+							TimeSpan wait = DateTime.Now - (startTime + span);
+							if (wait < TimeSpan.Zero)
+								break;
+
+							Monitor.Wait(_arplock, wait);
+						}
+
+						if (!_arptable.ContainsKey(dest)) {
+							throw new Exception("Couldn't find hardware address for " + dest);
+						}
+
+						hwaddr = _arptable[dest];
+					}
 				}
 			}
 
@@ -278,6 +311,37 @@ namespace Nabla {
 			return false;
 		}
 
+		private void sendARPRequest(IPAddress source, IPAddress dest) {
+			if (source.AddressFamily != AddressFamily.InterNetwork ||
+			    dest.AddressFamily != AddressFamily.InterNetwork) {
+				throw new Exception("Address of wrong type");
+			}
+
+			byte[] data = new byte[42];
+
+			/* Construct Ethernet header for broadcast address */
+			for (int i=0; i<6; i++)
+				data[i] = 0xff;
+			Array.Copy(_hwaddr, 0, data, 6, 6);
+			data[12] = 0x08;
+			data[13] = 0x06;
+
+			/* Construct ARP packet */
+			data[14] = 0x00;
+			data[15] = 0x01; // Hardware type: Ethernet
+			data[16] = 0x08;
+			data[17] = 0x00; // Protocol type: IP
+			data[18] = 6;    // Hardware size: 6
+			data[19] = 4;    // Protocol size: 4
+			data[20] = 0x00;
+			data[21] = 0x01; // Opcode: request
+			Array.Copy(_hwaddr, 0, data, 22, 6);
+			Array.Copy(source.GetAddressBytes(), 0, data, 28, 4);
+			Array.Copy(dest.GetAddressBytes(), 0, data, 38, 4);
+
+			_socket.Send(data);
+		}
+
 		private void handleARPRequest(byte[] data, int datalen) {
 			if (data[14] != 0x00 || data[15] != 0x01 || // Hardware type: Ethernet
 			    data[16] != 0x08 || data[17] != 0x00 || // Protocol type: IP
@@ -336,14 +400,77 @@ namespace Nabla {
 				}
 			}
 
-			if (_arptable.ContainsKey(addr)) {
-				Console.WriteLine("Hardware address for IP {0} already known", addr);
-				return;
+			lock (_arplock) {
+				if (_arptable.ContainsKey(addr)) {
+					Console.WriteLine("Hardware address for IP {0} already known", addr);
+					return;
+				}
+				_arptable.Add(addr, hwaddr);
+				Monitor.PulseAll(_arplock);
 			}
 
-			_arptable.Add(addr, hwaddr);
 			Console.WriteLine("Added hardware address {0} for IP address {1} into ARP table",
 				BitConverter.ToString(hwaddr).Replace('-', ':').ToLower(), addr);
+		}
+
+		private void sendNDSol(IPAddress source, IPAddress dest) {
+			if (source.AddressFamily != AddressFamily.InterNetworkV6 ||
+			    dest.AddressFamily != AddressFamily.InterNetworkV6) {
+				throw new Exception("Address of wrong type");
+			}
+
+			byte[] ipaddr = dest.GetAddressBytes();
+
+			/* Construct Ethernet header for Solicited-Node Address */
+			byte[] data = new byte[86];
+			data[0] = 0x33;
+			data[1] = 0x33;
+			data[2] = 0xff;
+			Array.Copy(ipaddr, 13, data, 3, 3);
+			Array.Copy(_hwaddr, 0, data, 6, 6);
+			data[12] = 0x86;
+			data[13] = 0xdd;
+
+			/* Construct IPv6 header for Solicited-Node Address */
+			int length = 8+16+8;
+			data[14] = 0x60;                  // IP version 6
+			data[18] = (byte) (length >> 8);
+			data[19] = (byte)  length;
+			data[20] = 58;                    // next header ICMPv6
+			data[21] = 255;                   // hop limit 255
+			Array.Copy(source.GetAddressBytes(), 0, data, 22, 16);
+			data[38] = 0xff;
+			data[39] = 0x02;
+			data[48] = 0x01;
+			data[49] = 0xff;
+			Array.Copy(ipaddr, 13, data, 50, 3);
+
+			/* Construct ICMPv6 packet with source link-layer address option */
+			data[54] = 135;
+			Array.Copy(ipaddr, 0, data, 62, 16);
+			data[78] = 1;
+			data[79] = 1;
+			Array.Copy(_hwaddr, 0, data, 80, 6);
+
+			/* Add pseudo-header into the checksum */
+			int checksum = 0;
+			checksum += data[14+4] << 8 | data[14+5];
+			checksum += data[14+6];
+			for (int i=0; i<32; i++)
+				checksum += data[14+8+i] << ((i%2 == 0)?8:0);
+
+			/* Checksum the actual data */
+			for (int i=0; i<length; i++)
+				checksum += data[14+40+i] << ((i%2 == 0)?8:0);
+
+			/* Store the final checksum into ICMPv6 packet */
+			if (checksum > 0xffff)
+				checksum = (checksum & 0xffff) + (checksum >> 16);
+			checksum = ~checksum;
+			data[14+40+2] = (byte) (checksum >> 8);
+			data[14+40+3] = (byte)  checksum;
+
+			_socket.Send(data);
 		}
 
 		private void handleNDSol(byte[] data, int datalen) {
@@ -387,6 +514,7 @@ namespace Nabla {
 			/* Add target link-layer address option */
 			data[14+40+8+16] = 2;
 			data[14+40+8+17] = 1;
+			Array.Copy(_hwaddr, 0, data, 80, 6);
 
 			/* Zero checksum */
 			int checksum = 0;
@@ -441,12 +569,16 @@ namespace Nabla {
 				}
 			}
 
-			if (_arptable.ContainsKey(addr)) {
-				Console.WriteLine("Hardware address for IP {0} already known", addr);
-				return;
+			lock (_arplock) {
+				if (_arptable.ContainsKey(addr)) {
+					Console.WriteLine("Hardware address for IP {0} already known", addr);
+					return;
+				}
+
+				_arptable.Add(addr, hwaddr);
+				Monitor.PulseAll(_arplock);
 			}
 
-			_arptable.Add(addr, hwaddr);
 			Console.WriteLine("Added hardware address {0} for IP address {1} into ARP table",
 				BitConverter.ToString(hwaddr).Replace('-', ':').ToLower(), addr);
 		}
