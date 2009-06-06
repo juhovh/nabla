5255c6bcbb1ecd11bd681c5bc4e419132db46096
juhovh <juhovh@37e7b450-0b1e-11de-ba4d-252e5d5a8945>
2009-06-06 13:35:00 +0000

    Add some more safety checks into TunnelSession

--- server/tests/TICServerTest.cs
+++ server/tests/TICServerTest.cs
@@ -3,7 +3,7 @@ using System.Net;
 using System.Net.Sockets;
 using Nabla;
 
-public class ParallelDeviceTest {
+public class TICServerTest {
 	private static void Main(string[] args) {
 		TICServer server = new TICServer();
 		server.Start();
